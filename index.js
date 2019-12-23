7e886b40c657b243c20cd6a92bb5013deb82ebfb
k4m4 <nikolaskam@gmail.com>
2019-12-22 22:55:48 -0500

    Fix styling issues

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-'use strict';
+'use strict'
 
-function buildRegExp(bodyExp, opts) {
-	let beginning = `\\b(?:`, end = `)\\b`
+const buildRegExp = (bodyExp, opts) => {
+	let beginning = '\\b(?:'
+	let end = ')\\b'
 	if (opts && opts.exact) {
-		beginning = `^(`
-		end = `)$`
+		beginning = '^('
+		end = ')$'
 	}
+
 	const regExp = beginning + bodyExp + end
 	if (opts && opts.exact) {
 		return new RegExp(regExp)
 	}
+
 	return new RegExp(regExp, 'g')
 }
 
 const hashLengthRegex = (bitLength, opts) => {
-  let RegExp = `(?:[a-f0-9]{` + bitLength/4 + `})`
+	const RegExp = `(?:[a-f0-9]{${bitLength / 4}})`
 	return buildRegExp(RegExp, opts)
 }
-module.exports = hashLengthRegex;
+
+module.exports = hashLengthRegex
